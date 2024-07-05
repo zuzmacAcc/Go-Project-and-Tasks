65bc92066da79aa3c0be192720783c886dc18221
zuzanna.macinska <zuzanna.macinska@accenture.com>
2024-07-05 12:26:17 +0200

    api + db connection - init

---
+++ db.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+type MySQLStorage struct {
+	db *sql.DB
+}
+
+func NewMySQLStorage(cfg mysql.Config) *MySQLStorage {
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connected to MySQL!")
+
+	return &MySQLStorage{db: db}
+}
+
+func (s *MySQLStorage) Init() (*sql.DB, error) {
+	// initialize the tables
+
+	return s.db, nil
+}
---
+++ store.go
@@ -0,0 +1,22 @@
+package main
+
+import "database/sql"
+
+type Store interface {
+	// Users
+	CreateUser() error
+}
+
+type Storage struct {
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Storage {
+	return &Storage{
+		db: db,
+	}
+}
+
+func (s *Storage) CreateUser() error {
+	return nil
+}
